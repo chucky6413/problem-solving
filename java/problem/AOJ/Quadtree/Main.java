d0cd2df0f88d8a6e9385bdfef76047e9fc09ec93
chucky6413 <chucky6413@naver.com>
2016-10-30 01:34:25 +0900

    Quadtree

---
+++ java/problem/AOJ/Quadtree/Main.java
@@ -0,0 +1,47 @@
+/*
+ * 출처 : http://timpani.tistory.com/entry/%EC%BF%BC%EB%93%9C-%ED%8A%B8%EB%A6%AC-%EB%92%A4%EC%A7%91%EA%B8%B0-QUADTREE
+ * 잘 보고 배웁니다.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Quadtree{  
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         
+        int T = Integer.parseInt(br.readLine());
+        while( T-- > 0 ){
+            String quadTree = new String(br.readLine());
+             
+            System.out.println(reverse(quadTree));
+        } 
+    }
+     
+    public static String reverse(String quadTree){
+        if( quadTree.charAt(0) != 'x' )
+            return quadTree.charAt(0) + "";
+         
+        // reverseQuadTree
+        String rqt[] = new String[4];
+         
+        // 왼쪽 위 조각
+        int beginIndex = 1;
+        rqt[0] = reverse(quadTree.substring(beginIndex));
+         
+        // 오른쪽 위 조각
+        beginIndex += rqt[0].length();
+        rqt[1] = reverse(quadTree.substring(beginIndex));
+         
+        // 왼쪽 아래 조각
+        beginIndex += rqt[1].length();
+        rqt[2] = reverse(quadTree.substring(beginIndex));
+         
+        // 오른쪽 아래 조각
+        beginIndex += rqt[2].length();
+        rqt[3] = reverse(quadTree.substring(beginIndex));
+         
+        return 'x' + rqt[2] + rqt[3] + rqt[0] + rqt[1];
+    }
+}
