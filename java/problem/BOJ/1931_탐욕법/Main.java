325cb43ba24976d11669566a9e50d861cfc0e26b
chucky6413 <chucky6413@naver.com>
2016-12-04 14:17:33 +0900

    commit

---
+++ java/problem/BOJ/1931_탐욕법/Main.java
@@ -0,0 +1,47 @@
+/*
+ * 2016.12.04
+ * Å½¿å¹ı
+ * ¹éÁØ 1931
+ */
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String arg[]){
+		Scanner sc = new Scanner(System.in);
+		int count=0;
+		int n = sc.nextInt();
+		PriorityQueue<Meeting> q = new PriorityQueue();
+
+		while( n-- > 0){
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			q.offer(new Meeting(start,end));
+		}
+
+		int lastEnd = -1;
+		while(!q.isEmpty()){
+			Meeting m = q.poll();
+			if(m.start>=lastEnd){
+				lastEnd=m.end;
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
+class Meeting implements Comparable<Meeting>{
+	int start;
+	int end;
+	
+	public Meeting(int start,int end){
+		this.start = start;
+		this.end = end;
+	}
+	public int compareTo(Meeting m){
+		if(end==m.end){
+			return start-m.start;
+		}
+		return end-m.end;
+	}
+}
