95ac9b7f5ca46f8b66f302a32879d60c8d73f5b5
chucky6413 <chucky6413@naver.com>
2016-12-28 01:26:51 +0900

    commit

---
+++ java/problem/BOJ/1260_DFS,BFS/Main.java
@@ -0,0 +1,63 @@
+/*
+ * 2016.12.28
+ * DFS,BFS
+ * น้มุ 1260
+ */
+import java.util.*;
+
+public class Main {
+	static int[][] adj;
+	static int[] visited;
+	static int n,m,start;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String arg[]){
+		Scanner input = new Scanner(System.in);
+		
+		n=input.nextInt();
+		m=input.nextInt();
+		start=input.nextInt();
+		
+		adj = new int[n+1][n+1];
+		for(int i=0;i<m;i++){
+			int a=input.nextInt();
+			int b=input.nextInt();
+			
+			adj[a][b] = adj[b][a] = 1;
+		}
+		
+		visited=new int[n+1];
+		dfs(start);
+		sb.append("\n");
+		visited=new int[n+1];
+		bfs(start);
+		
+		System.out.println(sb.toString());
+	}
+	public static void dfs(int s){
+		sb.append(s+" ");
+		visited[s]=1;
+		
+		for(int i=1;i<=n;i++){
+			if(visited[i]==0 && adj[s][i]==1){
+				dfs(i);
+			}
+		}
+	}
+	public static void bfs(int s){
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.offer(s);
+		
+		while(!queue.isEmpty()){
+			s = queue.poll();
+			sb.append(s+" ");
+			visited[s]=1;
+			
+			for(int i=1;i<=n;i++){
+				if(visited[i]==0 && adj[s][i]==1){
+					visited[i]=1;
+					queue.offer(i);
+				}
+			}
+		}
+	}
+}
