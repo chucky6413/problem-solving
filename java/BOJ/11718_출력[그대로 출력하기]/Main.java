5aee42c7b4c568795650c68d7466a3e8c23c621d
chucky6413 <chucky6413@naver.com>
2017-01-19 01:49:40 +0900

    commit

---
+++ java/BOJ/11718_출력[그대로 출력하기]/Main.java
@@ -0,0 +1,20 @@
+/*
+ * 2017.01.19
+ * Ãâ·Â
+ *
+ * @problem https://www.acmicpc.net/problem/11718
+ * @author chucky3
+ */
+import java.util.*;
+
+public class Main{
+    public static void main(String arg[]){
+        Scanner input = new Scanner(System.in);
+    	StringBuilder sb = new StringBuilder();
+        
+        while(input.hasNext()){
+        	sb.append(input.nextLine()+"\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
