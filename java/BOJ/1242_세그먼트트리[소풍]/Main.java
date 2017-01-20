7c8c7178ae726a3dbbb5f75c4c2e7f75a5d1a6f0
chucky6413 <chucky6413@naver.com>
2017-01-21 00:43:27 +0900

    commit

---
+++ java/BOJ/1242_세그먼트트리[소풍]/Main.java
@@ -0,0 +1,31 @@
+/*
+ * 2017.01.20
+ * 구현
+ *
+ * @problem https://www.acmicpc.net/problem/1242
+ * @author chucky3
+ */
+import java.util.*;
+
+public class Main{
+	public static void main(String arg[]){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int m = input.nextInt();
+        int k = input.nextInt();
+        
+        int cur=0;
+        int before =k-1;
+        
+        for(int i=0;i<n;i++){
+        	cur=(cur+m-1)%(n-i);
+        	if(cur==before){
+        		System.out.println(i+1);
+        		break;
+        	}
+        	if(cur<before){ //현재 위치보다 앞의 사람이 빠진다면, 내 위치도 줄어든다.
+        		before-=1;
+        	}
+        }
+    }
+}
---
+++ java/BOJ/1592_시뮬레이션[영식이와 친구들]/Main.java
@@ -0,0 +1,34 @@
+/*
+ * 2017.01.20
+ * 구현
+ *
+ * @problem https://www.acmicpc.net/problem/1592
+ * @author chucky3
+ */
+import java.util.*;
+
+public class Main{
+	public static void main(String arg[]){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int m = input.nextInt();
+        int l = input.nextInt();
+        
+        int count[] = new int[n];
+        int result = 0,pick = 0;
+        count[0]=1;
+        
+        while(true){
+        	if(count[pick]==m){ 
+        		break;
+        	}else if(count[pick]%2==0){
+        		pick=(pick+l)%n;
+        	}else{
+        		pick=((pick-l)%n+n)%n;
+        	}
+        	count[pick]++;
+        	result++; //결과 횟수 증가
+        }
+        System.out.println(result);
+    }
+}
