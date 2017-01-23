bf4c14af155770db7d303142a0e1f8a0f4105158
chucky6413 <chucky6413@naver.com>
2017-01-23 22:33:02 +0900

    commit

---
+++ java/BOJ/10942_동적계획[팰린드롬]/Main.java
@@ -0,0 +1,48 @@
+/*
+ * 2017.01.23
+ * 동적계획
+ *
+ * @problem https://www.acmicpc.net/problem/10942
+ * @author chucky3
+ */
+import java.util.Arrays;
+import java.io.*;
+public class Main {
+	static int arr[],dp[][];
+	public static void main(String arg[]) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		String temp[];
+		
+		int n = Integer.parseInt(br.readLine());
+		arr = new int[n+1];
+		dp = new int[n+1][n+1];
+		temp = br.readLine().split(" ");
+		for(int i=1;i<=n;i++) arr[i] = Integer.parseInt(temp[i-1]);
+		for(int i=1;i<=n;i++) Arrays.fill(dp[i], -1);
+		
+		int m = Integer.parseInt(br.readLine());
+		for(int i=0;i<m;i++){
+			temp = br.readLine().split(" ");
+			int s = Integer.parseInt(temp[0]);
+			int e = Integer.parseInt(temp[1]);
+			int result = solve(s,e);
+			sb.append(result+"\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	public static int solve(int front,int back){
+		if(front>=back) return 1;
+		if(dp[front][back]==-1){
+			dp[front][back] = 0;
+			if(arr[front]==arr[back]){
+				if(solve(front+1,back-1)==1){ //팰린드롬의, 내부도 팰린드롬 조건이 맞을 때
+					dp[front][back] = 1; //팰린드롬이다.
+				}
+			}
+		}
+		return dp[front][back];
+	}
+}
---
+++ java/BOJ/2631_동적계획[줄세우기]/Main.java
@@ -0,0 +1,29 @@
+/*
+ * 2017.01.23
+ * 동적계획
+ *
+ * @problem https://www.acmicpc.net/problem/2631
+ * @author chucky3
+ */
+import java.util.Scanner;
+public class Main {
+	static int arr[],dp[],max=0;
+	public static void main(String arg[]){
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		arr = new int[n];
+		dp = new int[n];
+		for(int i=0;i<n;i++) arr[i] = input.nextInt();
+		// longest increasing subsequence
+		for(int i=0;i<n;i++){
+			for(int j=0;j<i;j++){
+				if(arr[i]>arr[j] && dp[i]<dp[j]+1){
+					dp[i] = dp[j]+1;
+				}
+			}
+			if(max<dp[i])
+				max=dp[i];
+		}
+		System.out.println((n-1)-max); //dp[0] = 0 부터 시작하므로, lis 가 -1만큼 작기 때문에 -1을 더 빼준다.
+	}
+}
