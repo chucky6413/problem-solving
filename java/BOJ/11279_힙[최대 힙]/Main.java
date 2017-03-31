0799d43e6b2d11ed0a943a06daa6afb5a16981f7
chucky6413 <chucky6413@naver.com>
2017-03-31 14:19:44 +0900

    위상정렬

---
+++ java/BOJ/11279_힙[최대 힙]/Main.java
@@ -0,0 +1,31 @@
+/*
+ * 2017.03.31
+ * 최대 힙
+ * 
+ * @problem https://www.acmicpc.net/problem/11279
+ * @github https://github.com/chucky6413
+ * @author chucky3
+ */
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n;
+	static final String NEWLINE = "\n";
+	public static void main(String arg[]) throws IOException{
+		Scanner input = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		n = input.nextInt();
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>(1,Collections.reverseOrder()); //reverse로 구현
+		for(int i=0;i<n;i++){
+			int num = input.nextInt();
+			if(num==0){
+				if(queue.isEmpty()) sb.append("0"+NEWLINE);
+				else sb.append(queue.poll()+NEWLINE);
+			}else{
+				queue.offer(num);
+			}
+		}
+		System.out.println(sb);
+	}
+}
