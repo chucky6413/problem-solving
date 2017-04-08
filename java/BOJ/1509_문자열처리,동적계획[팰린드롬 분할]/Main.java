4cd32cc415997a681edef4aa5a4a1dfe4be87f8f
chucky6413 <chucky6413@naver.com>
2017-04-08 12:28:33 +0900

    팰린드롬 분할

---
+++ java/BOJ/1509_문자열처리,동적계획[팰린드롬 분할]/Main.java
@@ -0,0 +1,54 @@
+/*
+ * 2017.04.08
+ * 문자열 처리, 동적계획
+ * 
+ * @problem https://www.acmicpc.net/problem/1509
+ * @github https://github.com/chucky6413
+ * @author chucky3
+ */
+import java.util.*;
+
+public class Main{
+	static int dp[][] = new int[2500][2500];
+	static int result[] = new int[2500];
+	static char str[] = new char[2500];
+	public static void main(String arg[]){
+		Scanner input = new Scanner(System.in);
+		str = input.nextLine().toCharArray();
+		int size = str.length;
+		
+		for(int i=0;i<size;i++){
+			dp[i][i] = 1;
+		}
+		
+		for(int i=0;i<size-1;i++){
+			if(str[i]==str[i+1]){
+				dp[i][i+1] = 1;
+			}
+		}
+		
+		for(int i=2;i<size;i++){
+			for(int j=0;i+j<size;j++){
+				if(str[j]==str[i+j] && dp[j+1][i+j-1]==1){
+					dp[j][j+i] = 1;
+				}
+			}
+		}
+		
+		for(int i=0;i<size;i++){
+			if(i==0)
+				result[i] = 1;
+			else if((result[i]!=0 && result[i]>result[i-1]+1) || result[i]==0)
+				result[i] = result[i-1] + 1;
+			
+			for(int j=i+1;j<size;j++){
+				if(dp[i][j]!=0){
+					if((result[j]!=0 && result[j]>result[i-1]+1) || result[j]==0){
+						result[j] = i==0? 1 : result[i-1] + 1;
+					}
+				}
+			}
+		}
+		System.out.println(result[size-1]);
+	}
+}
