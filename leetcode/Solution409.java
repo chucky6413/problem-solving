76c7422e60f3c8718c51b9a8a00716b6c9579e4a
junwoo-lee <junwoo.lee@navercorp.com>
2018-09-30 02:01:22 +0900

    solve 409.Longest Palindrome(easy) of leetCode

---
+++ leetcode/Solution409.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution409 {
+    public int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!set.add(s.charAt(i))) {
+                set.remove(c);
+                count += 2;
+            }
+        }
+
+        if (!set.isEmpty()) {
+            count++;
+        }
+
+        return count;
+    }
+}
