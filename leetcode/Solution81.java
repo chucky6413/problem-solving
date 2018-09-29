3e5c6cb6c69d02e3f3ba93c51454b2945de1a3dd
junwoo-lee <junwoo.lee@navercorp.com>
2018-09-29 09:49:21 +0900

    solve 81.Search in Rotated Sorted Array II(medium) of leetCode

---
+++ leetcode/Solution81.java
@@ -0,0 +1,36 @@
+public class Solution81 {
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[left] < nums[mid]) {
+
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            } else if (nums[left] > nums[mid]) {
+
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+
+            } else {
+                left++;
+            }
+        }
+
+        return false;
+    }
+}
