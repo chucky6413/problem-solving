d329ffa9c0ae932bdddace97d9abeb56b8a960eb
junwoo-lee <junwoo.lee@navercorp.com>
2018-10-25 21:59:35 +0900

    update code 77.Combinations(medium) of leetCode

--- leetcode/Solution77.java
+++ leetcode/Solution77.java
@@ -1,31 +1,23 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Solution77 {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> rs = new ArrayList<>();
-        LinkedList<Integer> subList = new LinkedList<>();
-
-        for (int i = 1; i <= n; i++) {
-            subList.add(i);
-            recur(rs, i + 1, n, k, subList);
-            subList.removeLast();
-        }
-
+        recur(rs, 1, n, k, new ArrayList<>());
         return rs;
     }
 
-    public void recur(List<List<Integer>> rs, int cur, int n, int k, LinkedList<Integer> subList) {
+    public void recur(List<List<Integer>> rs, int cur, int n, int k, List<Integer> subList) {
         if (subList.size() == k) {
-            rs.add(new LinkedList<>(subList));
+            rs.add(new ArrayList<>(subList));
             return;
         }
 
         for (int i = cur; i <= n; i++) {
             subList.add(i);
             recur(rs, i + 1, n, k, subList);
-            subList.removeLast();
+            subList.remove(subList.size() - 1);
         }
     }
 }
