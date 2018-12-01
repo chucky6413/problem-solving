ed67fb649e1d016fadf7560f276f6a6ca7809acf
junwoo-lee <junwoo.lee@navercorp.com>
2018-12-02 02:05:41 +0900

    solve 9.Palindrome Number(easy) of leetCode

---
+++ leetcode/Solution9.java
@@ -0,0 +1,16 @@
+public class Solution9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reverse = 0;
+        int temp = x;
+        while (temp > 0) {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        return x == reverse;
+    }
+}
