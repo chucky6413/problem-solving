a149851dd70ca9eb51bcc44cd6c249df6cc12f7a
junwoo-lee <junwoo.lee@navercorp.com>
2018-12-25 18:13:42 +0900

    solve 342. Power of Four(easy) of leetCode

---
+++ leetcode/Solution342.java
@@ -0,0 +1,20 @@
+public class Solution342 {
+    public boolean isPowerOfFour(int num) {
+        int max4power = getMax4Power(num);
+
+        if (num <= 0 || num > max4power || (max4power % num != 0)) return false;
+
+        while (num > 1) {
+            num >>= 2;
+        }
+        return num == 1;
+    }
+
+    private int getMax4Power(int n) {
+        long v = 4;
+        while (v < Integer.MAX_VALUE) {
+            v *= 4;
+        }
+        return (int) (v / 4);
+    }
+}
