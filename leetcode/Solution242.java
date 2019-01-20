48693efdaa97ede41b06e377d1d792b14b9b67d3
junwoo-lee <junwoo.lee@navercorp.com>
2019-01-21 00:36:14 +0900

    solve 242. Valid Anagram(easy) of leetCode

---
+++ leetcode/Solution242.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+        return true;
+    }
+}
