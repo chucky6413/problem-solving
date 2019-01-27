5a949e4c2a0e772b4c6d8df360147fc82af12d98
junwoo-lee <junwoo.lee@navercorp.com>
2019-01-27 21:32:10 +0900

    solve 19. Remove Nth Node From End of List(medium) of leetCode

---
+++ leetcode/Solution19.java
@@ -0,0 +1,23 @@
+public class Solution19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int length = 0;
+
+        ListNode node = head;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+
+        if (length == n) {
+            return head.next;
+        }
+
+        n = length - n;
+        node = head;
+        while (--n > 0) {
+            node = node.next;
+        }
+        node.next = node.next.next;
+        return head;
+    }
+}
