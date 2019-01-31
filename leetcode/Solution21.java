d2eee77f715681d9c8259045bd1942ca4c1255af
junwoo-lee <junwoo.lee@navercorp.com>
2019-01-31 20:45:14 +0900

    solve 21. Merge Two Sorted Lists(easy) of leetCode

--- leetcode/Solution21.java
+++ leetcode/Solution21.java
@@ -1,27 +1,27 @@
 public class Solution21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode rs = new ListNode(0);
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
 
         while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                rs.next = new ListNode(l1.val);
+            if (l1.val < l2.val) {
+                temp.next = l1;
                 l1 = l1.next;
             } else {
-                rs.next = new ListNode(l2.val);
+                temp.next = l2;
                 l2 = l2.next;
             }
+            temp = temp.next;
         }
 
-        while (l1 != null) {
-            rs.next = new ListNode(l1.val);
-            l1 = l1.next;
+        if (l1 != null) {
+            temp.next = l1;
         }
 
-        while (l2 != null) {
-            rs.next = new ListNode(l2.val);
-            l2 = l2.next;
+        if (l2 != null) {
+            temp.next = l2;
         }
 
-        return rs.next;
+        return head.next;
     }
 }
