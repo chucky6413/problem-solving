94e242462afcc570c14630166433db837cc2a997
junwoo-lee <junwoo.lee@navercorp.com>
2019-02-01 02:08:28 +0900

    solve 118. Pascal's Triangle(easy) of leetCode

---
+++ leetcode/Solution118.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
