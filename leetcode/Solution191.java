65de9b7b75f214eb55cc437cf1cda61ac7491c95
junwoo-lee <junwoo.lee@navercorp.com>
2019-02-04 01:42:59 +0900

    solve 191. Number of 1 Bits(easy) of leetCode

---
+++ leetcode/Solution191.java
@@ -0,0 +1,10 @@
+public class Solution191 {
+    public int hammingWeight(int n) {
+        int bc = 0;
+        for (int i = 0; i < 32; i++) {
+            bc += n % 2;
+            n >>= 1;
+        }
+        return Math.abs(bc);
+    }
+}
