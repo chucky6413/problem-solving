c9c42574f855a873ca0c273468d4b9380d491341
junwoo-lee <junwoo.lee@navercorp.com>
2019-02-03 17:36:40 +0900

    solve 121. Best Time to Buy and Sell Stock(easy) of leetCode

--- leetcode/Solution121.java
+++ leetcode/Solution121.java
@@ -1,20 +1,13 @@
 public class Solution121 {
     public int maxProfit(int[] prices) {
-        if (prices.length < 2) {
-            return 0;
-        }
-
-        int res = 0;
-        int minValue = Integer.MAX_VALUE;
-
-        for (int price : prices) {
-            if (price < minValue) {
-                minValue = price;
-            } else if (price - minValue > res) {
-                res = price - minValue;
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] - min > max) {
+                max = prices[i] - min;
             }
+            min = Math.min(min, prices[i]);
         }
-
-        return res;
+        return max;
     }
 }
