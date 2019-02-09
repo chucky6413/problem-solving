2584d3ba1c648fc4d055c963d639145a1184b519
junwoo-lee <junwoo.lee@navercorp.com>
2019-02-10 02:46:59 +0900

    solve 49. Group Anagrams(medium) of leetCode

---
+++ leetcode/Solution49.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] cArr = s.toCharArray();
+            Arrays.sort(cArr);
+            String v = Arrays.toString(cArr);
+
+            if (!map.containsKey(v)) {
+                map.put(v, new ArrayList<>());
+            }
+            map.get(v).add(s);
+        }
+
+        List<List<String>> rs = new ArrayList<>();
+        for (List<String> sub : map.values()) {
+            rs.add(sub);
+        }
+        return rs;
+    }
+}
