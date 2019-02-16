741ee4a4b5c18e33e490790daa36b25c1832eb35
junwoo-lee <junwoo.lee@navercorp.com>
2019-02-16 23:41:06 +0900

    solve 334. Increasing Triplet Subsequence(medium) of leetCode

---
+++ leetcode/Solution334.java
@@ -0,0 +1,24 @@
+public class Solution334 {
+    public boolean increasingTriplet(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (first >= num) {
+                first = num;
+            } else if (second > num) {
+                second = num;
+            }
+
+            if (second < num) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
