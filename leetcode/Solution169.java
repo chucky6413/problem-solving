e55e51e5890096a75c1d4da7a7fd0deca41a8a01
junwoo-lee <junwoo.lee@navercorp.com>
2019-02-16 15:05:08 +0900

    solve 169. Majority Element(easy) of leetCode

---
+++ leetcode/Solution169.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution169 {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int majorElm = 0;
+        for (int num : nums) {
+            if (!countMap.containsKey(num)) {
+                countMap.put(num, 0);
+            }
+            countMap.put(num, countMap.get(num) + 1);
+            majorElm = countMap.getOrDefault(majorElm, 0) > countMap.get(num) ? majorElm : num;
+        }
+        return majorElm;
+    }
+}
