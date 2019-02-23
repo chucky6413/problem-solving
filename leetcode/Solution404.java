d3474a5bc8634ff99f033cf90b6e817653d713ac
junwoo-lee <junwoo.lee@navercorp.com>
2019-02-23 14:37:21 +0900

    solve 404. Sum of Left Leaves(easy) of leetCode

--- leetcode/Solution404.java
+++ leetcode/Solution404.java
@@ -1,17 +1,12 @@
 public class Solution404 {
     public int sumOfLeftLeaves(TreeNode root) {
-        return traverse(root);
-    }
-
-    private int traverse(TreeNode node) {
-        if (node == null) {
+        if (root == null) {
             return 0;
         }
-
         int rs = 0;
-        if (node.left != null && node.left.left == null && node.left.right == null) {
-            rs += node.left.val;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            rs += root.left.val;
         }
-        return rs + traverse(node.left) + traverse(node.right);
+        return rs + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
     }
 }
