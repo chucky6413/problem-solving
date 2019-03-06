3085964608fbd68804f1e909df45803a10776ef7
junwoo-lee <junwoo.lee@navercorp.com>
2019-03-07 01:15:56 +0900

    solve 343. Integer Break(medium) of leetCode

---
+++ leetcode/Solution343.java
@@ -0,0 +1,12 @@
+public class Solution343 {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
