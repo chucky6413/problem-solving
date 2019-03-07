d4c1bcc35b16abd7fad6bc41fecca6ffba2bbacd
junwoo-lee <junwoo.lee@navercorp.com>
2019-03-08 01:29:44 +0900

    solve 148. Sort List(medium) of leetCode

---
+++ leetcode/Solution148.java
@@ -0,0 +1,52 @@
+public class Solution148 {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prevCenter = findPrevCenter(head);
+        ListNode center = prevCenter.next;
+        prevCenter.next = null;
+        head = merge(sortList(head), sortList(center));
+        return head;
+    }
+
+    private ListNode findPrevCenter(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode first, ListNode center) {
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+
+        while (first != null || center != null) {
+            if (first.val < center.val) {
+                tail.next = first;
+                first = first.next;
+            } else {
+                tail.next = center;
+                center = center.next;
+            }
+
+            tail = tail.next;
+
+            if (first == null) {
+                tail.next = center;
+                break;
+            }
+
+            if (center == null) {
+                tail.next = first;
+                break;
+            }
+        }
+
+        return head.next;
+    }
+}
