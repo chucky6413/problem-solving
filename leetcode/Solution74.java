7eb405087b90ac52b9fe2da23c03c0db894b3cee
junwoo-lee <junwoo.lee@navercorp.com>
2019-04-07 18:06:44 +0900

    solve 74. Search a 2D Matrix(medium) of leetCode

---
+++ leetcode/Solution74.java
@@ -0,0 +1,31 @@
+public class Solution74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int r = mid / n;
+            int c = mid % n;
+
+            if (matrix[r][c] == target) {
+                return true;
+            }
+
+            if (matrix[r][c] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
