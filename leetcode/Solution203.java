9424490003620386734878253adc721d8fce6518
junwoo-lee <junwoo.lee@navercorp.com>
2019-04-09 20:37:25 +0900

    solve 203. Remove Linked List Elements(easy) of leetCode

---
+++ leetcode/Solution203.java
@@ -0,0 +1,17 @@
+public class Solution203 {
+    public ListNode removeElements(ListNode node, int val) {
+        ListNode head = new ListNode(0);
+        head.next = node;
+        node = head;
+
+        while (node.next != null) {
+            if (node.next.val == val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+
+        return head.next;
+    }
+}
