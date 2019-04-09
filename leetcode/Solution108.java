8ad376bfb3ab31867282ff5963ea7695750ad739
junwoo-lee <junwoo.lee@navercorp.com>
2019-04-09 21:30:58 +0900

    solve 108. Convert Sorted Array to Binary Search Tree(easy) of leetCode

--- leetcode/Solution108.java
+++ leetcode/Solution108.java
@@ -1,16 +1,17 @@
 public class Solution108 {
     public TreeNode sortedArrayToBST(int[] nums) {
-        return makeTree(null, nums, 0, nums.length - 1);
+        return makeTree(nums, 0, nums.length - 1);
     }
 
-    private TreeNode makeTree(TreeNode node, int[] nums, int left, int right) {
+    private TreeNode makeTree(int[] nums, int left, int right) {
         if (left > right) {
-            return node;
+            return null;
         }
-        int mid = (left + right) / 2;
-        node = new TreeNode(nums[mid]);
-        node.left = makeTree(node.left, nums, left, mid - 1);
-        node.right = makeTree(node.right, nums, mid + 1, right);
+        int mid = left + (right - left) / 2;
+
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = makeTree(nums, left, mid - 1);
+        node.right = makeTree(nums, mid + 1, right);
         return node;
     }
 }
