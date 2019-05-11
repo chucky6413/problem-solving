a5222007aea7297ab36956c49c29afa72f1d6137
junwoo-lee <junwoo.lee@navercorp.com>
2019-05-12 01:28:28 +0900

    solve 242. Valid Anagram(easy) of leetCode

--- leetcode/Solution242.java
+++ leetcode/Solution242.java
@@ -1,25 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution242 {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+        int[] count = new int[27];
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
         }
 
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
         for (char c : t.toCharArray()) {
-            if (!map.containsKey(c)) {
+            count[c - 'a']--;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
                 return false;
             }
-            map.put(c, map.get(c) - 1);
-            if (map.get(c) == 0) {
-                map.remove(c);
-            }
         }
+
         return true;
     }
 }
