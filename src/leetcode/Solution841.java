871c9b7efc7cfd2c64e00223def5bfb17af88cef
junwoo-lee <junwoo.lee@navercorp.com>
2019-10-04 23:05:55 +0900

    [java] 841. Keys and Rooms(medium) of leetCode

---
+++ src/leetcode/Solution841.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Solution841 {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        visited[0] = true;
+        int c = 1;
+        while (!stack.isEmpty()) {
+            int roomNumber = stack.pop();
+            for (int key : rooms.get(roomNumber)) {
+                if (!visited[key]) {
+                    c++;
+                    visited[key] = true;
+                    stack.push(key);
+                }
+            }
+        }
+
+        return c == rooms.size() ? true : false;
+    }
+}
