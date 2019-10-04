a6a29325bb8ea12b9defb9bc35226ffe663c4644
junwoo-lee <junwoo.lee@navercorp.com>
2019-10-04 22:03:55 +0900

    [java] 338. Counting Bits(medium) of leetCode

---
+++ src/leetcode/Solution338.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Solution338 {
+    public int[] countBits(int num) {
+        int[] dp = new int[num + 1];
+        int offset = 1;
+        for (int i = 1; i <= num; i++) {
+            if ((offset * 2) == i) {
+                offset *= 2;
+            }
+            dp[i] = dp[i - offset] + 1;
+        }
+        return dp;
+    }
+}
