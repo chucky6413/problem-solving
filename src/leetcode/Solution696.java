c352ce2c89f38ce50812373d11fe09234b107023
junwoo-lee <junwoo.lee@navercorp.com>
2019-11-06 23:14:23 +0900

    [java] add 696. Count Binary Substrings(easy) of leetCode

---
+++ src/leetcode/Solution696.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class Solution696 {
+    public int countBinarySubstrings(String s) {
+        int res = 0;
+        int cur = 1;
+        int prev = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                cur++;
+            } else {
+                prev = cur;
+                cur = 1;
+            }
+            if (prev >= cur) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
