1b7b24b4e86cd28f9a34564c136b7459a6d9ded4
junwoo-lee <junwoo.lee@navercorp.com>
2019-11-12 00:02:34 +0900

    [java] add 1239. Maximum Length of a Concatenated String with Unique Characters(medium) of leetCode

---
+++ src/leetcode/Solution1239.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution1239 {
+    public int maxLength(List<String> arr) {
+        List<Integer> dp = new ArrayList<>();
+        dp.add(0);
+        int res = 0;
+        for (String s : arr) {
+            int bits = 0;
+            boolean dup = false;
+            for (char c : s.toCharArray()) {
+                int b = 1 << (c - 'a');
+                dup = dup ? dup : (bits & b) > 0;
+                bits |= b;
+            }
+            if (dup) {
+                continue;
+            }
+            for (int i = 0; i < dp.size(); i++) {
+                if ((bits & dp.get(i)) > 0) {
+                    continue;
+                }
+                dp.add(bits | dp.get(i));
+                res = Math.max(res, Integer.bitCount(bits | dp.get(i)));
+            }
+        }
+        return res;
+    }
+}
