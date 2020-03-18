53bce7d44fbd02fda952b6bdb715752f8b322c60
junwoo-lee <junwoo.lee@navercorp.com>
2020-03-18 10:24:33 +0900

    update kickstart template

--- src/codejam/kickstart/KickStartTemplate.java
+++ src/codejam/kickstart/KickStartTemplate.java
@@ -1,23 +1,32 @@
 package codejam.kickstart;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class KickStartTemplate {
-	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static final String SPACE = " ";
-	private static final String NEW_LINE = "\n";
+	//	private static final Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+	private static final PrintWriter w = new PrintWriter(System.out);
 	private static final StringBuilder rs = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		int t = Integer.parseInt(br.readLine());
+	private static final String SPACE = " ";
+	private static final String NEW_LINE = "\n";
+
+	public static void main(String[] args) throws FileNotFoundException {
+		final Scanner sc = new Scanner(
+			new BufferedReader(new FileReader(new File(".").getAbsolutePath() + "/src/codejam/input.txt")));
+
+		int t = sc.nextInt();
 		for (int i = 1; i <= t; i++) {
 			rs.append(String.format("Case #%d: ", i))
 				.append(solve())
 				.append(NEW_LINE);
 		}
-		System.out.println(rs.toString());
+		w.print(rs.toString());
+		w.close();
 	}
 
 	private static int solve() {
