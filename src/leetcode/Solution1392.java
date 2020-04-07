87cf82f0bf8cc1301dfb303b88f1fc373ffd190a
junwoo-lee <junwoo.lee@navercorp.com>
2020-04-08 01:56:12 +0900

    [leetcode] refactor 1392. Longest Happy Prefix(hard)

--- src/leetcode/Solution1392.java
+++ src/leetcode/Solution1392.java
@@ -2,16 +2,23 @@ package leetcode;
 
 public class Solution1392 {
 	public String longestPrefix(String s) {
-		int[] m = new int[s.length()];
+		int sameLen = getPi(s);
+		return s.substring(0, sameLen);
+	}
+
+	private int getPi(String s) {
+		int[] pi = new int[s.length()];
+
 		int j = 0;
 		for (int i = 1; i < s.length(); i++) {
 			if (s.charAt(i) == s.charAt(j)) {
-				m[i] = ++j;
+				pi[i] = ++j;
 			} else if (j > 0) {
-				j = m[j - 1];
+				j = pi[j - 1];
 				--i;
 			}
 		}
-		return s.substring(0, j);
+
+		return pi[pi.length - 1];
 	}
 }
